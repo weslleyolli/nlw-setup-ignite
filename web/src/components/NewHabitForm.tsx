8bfbf2df5bb807c1893c468a533ab693cd67677e
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-01-28 23:17:53 -0300

    Creation of new habit

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -30,6 +30,9 @@ export function NewHabitForm() {
             weekDays,
         })
 
+        setTitle('')
+        setWeekDays([])
+
         alert('Habit created with sucess')
     }
 
@@ -57,6 +60,7 @@ export function NewHabitForm() {
                 placeholder="ex.: Exercise, Sleep well, etc..."
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
                 autoFocus
+                value={title}
                 onChange={event => setTitle(event.target.value)}
             />
 
@@ -71,6 +75,7 @@ export function NewHabitForm() {
                         <Checkbox.Root 
                             key={weekDay} 
                             className='flex items-center gap-3 group'
+                            checked={weekDays.includes(index)}
                             onCheckedChange={() => {handleToggleWeekDay(index)}}
                         >
                             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
